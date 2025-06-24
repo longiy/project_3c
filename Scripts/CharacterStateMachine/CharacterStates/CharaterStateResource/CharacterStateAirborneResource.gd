09e28ca534f4645e6801efc6fc19ef795e899175
longiy <46914861+longiy@users.noreply.github.com>
2025-06-24 10:09:46 +0200

    .

--- Scripts/CharacterStateMachine/CharacterStates/CharaterStateResource/CharacterStateAirborneResource.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterState
-class_name CharacterStateAirborneResource
-
-@export_group("Airborne Specific")
-@export var gravity_multiplier: float = 1.0
-@export var terminal_velocity: float = 20.0
-@export var air_jump_velocity: float = 5.0
-@export var landing_buffer_time: float = 0.1
